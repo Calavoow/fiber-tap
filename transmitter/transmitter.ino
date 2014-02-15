3d2791ba1ffb5c04238bd4b7ada44dbf8ff15cfb
Calavoow <Calavoow@users.noreply.github.com>
2014-02-15 19:56:14 +0100

    Implemented transmitter with Timer

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,13 +1,33 @@
+#include <Event.h>
+#include <Timer.h>
+
 // LED pin
 int led = 2;
+short ledCounter = 0;
+boolean ledOn = false;
+
+// Timers
+Timer ledTimer;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
+  ledTimer.every(20, updateLed);
+}
+
+void updateLed() {
+  ledCounter++;
+  
+  if(ledOn){
+    digitalWrite(led, LOW);
+    ledOn = false;
+    ledCounter = 0;
+  } else if(ledCounter == 50) {
+    digitalWrite(led,HIGH);
+    ledOn = true;
+  }
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(20);
-  digitalWrite(led, LOW);
-  delay(1000);
+  ledTimer.update();
 }
