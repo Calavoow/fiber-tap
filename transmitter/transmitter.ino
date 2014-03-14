4f39c34f004c5bed1b519eeec24020156ea881d2
Calavoow <Calavoow@users.noreply.github.com>
2014-03-14 20:22:27 +0100

    Cleaned up some old code.

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -14,63 +14,17 @@ Timer TXTimer;
 // Photoresistor pin
 int photoSensor = A0;
 
-unsigned char data = 0xA1;
-char transmitCounter = 0;
-unsigned char transmitData[16];
-
-void transmit(){
-  if(transmitCounter >= 16) {
-    transmitCounter = 0;
-    return;
-  }
-  
-  Serial.println("Transmitting!");
-  Serial.println(millis());
-  unsigned char data = transmitData[transmitCounter];
-  Serial.println(data);
-  if(data){
-    Serial.println("High");
-    digitalWrite(led, HIGH);
-  } else {
-    Serial.println("Low");
-    digitalWrite(led,LOW);
-  }
-  
-  transmitCounter++;
-}
-
-void manchesterData(byte data) {;
-  int i = 0;
-    for(; i < 8; i+=2){
-      unsigned char bit = data & (1 << i);
-      if( bit ){
-        //high-to-low
-	transmitData[i] = 1;
-	transmitData[i+1] = 0;
-      } else {
-	//low-to-high
-        transmitData[i] = 0;
-        transmitData[i+1] = 1;
-      }
-    }
-}
-
 #define BUFFER_SIZE 10
 uint8_t buffer[BUFFER_SIZE];
 void setup() {
   Serial.begin(9600);
   
-  //pinMode(led, OUTPUT);
-  
   //Use a pullup as input pin for the photoresistor.
   man.setupTransmit(led, MAN_19200);
-  pinMode(photoSensor, INPUT_PULLUP);
   man.setupReceiveAnalog(photoSensor, MAN_19200, 200);
   man.beginReceive();
   
-  
   TXTimer.every(500, transmitDataFunc);
-  // manchesterData(data);
   
   // Wait for Serial to connect to so output.
   while(!Serial) ;
