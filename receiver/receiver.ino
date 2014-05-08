594124f2bf4fed4e859fcd81a6d66c26de381682
Calavoow <Calavoow@users.noreply.github.com>
2014-05-08 14:45:13 +0200

    Working transmission over fiber cable

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,48 +1,84 @@
 #include <Manchester.h>
 
 // Photoresistor pin
+
 int photoSensor = A0;
 
 void setup() {
 	Serial.begin(9600);
-	
-	man.setupReceiveAnalog(photoSensor, MAN_300, 350);
-	
+
+	// Use analog pin as digital
+	man.setupReceive(photoSensor, MAN_300);
+//		  pinMode(photoSensor, INPUT);
+		
 	// Wait for Serial to connect to so output.
 	while(!Serial) ;
 	Serial.println("begin");
 
-        man.beginReceive();
+		man.beginReceive();
 }
 
 /**
  * Read a sensor value. Used for debugging the threshold.
  **/
+ /*
 int minimum = 30000;
+int maximum = 0;
+int iteration = 0;
 void readSensor() {
 	int sensorVal = analogRead(photoSensor);
-	Serial.print("Sensor Value:\t");
-	Serial.println(sensorVal);
 	minimum = min(minimum, sensorVal);
-	Serial.print("Minimum Value:\t");
-	Serial.println(minimum);
-	
-//	if(sensorVal < 200) {
-//		Serial.println(millis());
-//		Serial.println("On!");
-//	}
+	maximum = max(maximum, sensorVal);
+	if(iteration > 1000){
+		Serial.print("Maximum:");
+		Serial.print(maximum);
+		Serial.print("\tMinimum: ");
+		Serial.print(minimum);
+		Serial.println("");
+		iteration = 0;
+	}
+	iteration++;
 }
+*/
 
+// Buffer the received messages because printing corrupts the next message.
+uint16_t msgs[128];
+int msg_counter = 0;
 /**
  * The main receiver program loop.
  **/
 void loop() {
-//    readSensor();
+	/**
+	 * Debug transfer
+	int meas[128];
+	int meas2[128];
+	for(int i=0; i<128; i++){
+		meas[i] = digitalRead(photoSensor);
+		meas2[i] = analogRead(photoSensor);
+	}
+ 
+	for(int i=0; i<128; i++){
+	   Serial.print(meas[i]);
+	   Serial.print("\t");
+	   Serial.println(meas2[i]);
+	}
+	Serial.println("-----------------");
+	**/
+
 	// Receive data packets.
 	if(man.receiveComplete()) {
 		uint16_t m = man.getMessage();
 		// Get ready to receive next packet
 		man.beginReceive();
-		Serial.println(m);
+		msgs[msg_counter] = m;
+		msg_counter++;
+			
+		// Print received messages
+		if(msg_counter > 127){
+			for(int i=0; i<128; i++){
+				Serial.println(msgs[i]);
+			}
+			msg_counter = 0;
+		}
 	}
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -4,21 +4,20 @@
 int led = 4;
 
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
   
-  man.setupTransmit(led, MAN_300);
-  
-  // Wait for Serial to connect to so output.
-  while(!Serial) ;
-  Serial.println("begin");
+	man.setupTransmit(led, MAN_300); 
+	// Wait for Serial to connect to so output.
+	while(!Serial) ;
+	Serial.println("begin");
 }
 
 int16_t counter = 1;
 void loop() {
-  // Transmit some data
-    Serial.println(counter);
+	// Transmit some data
+	Serial.println(counter);
   
-    man.transmit(counter);
+	man.transmit(counter);
 
-    counter++;
+	counter++;
 }
