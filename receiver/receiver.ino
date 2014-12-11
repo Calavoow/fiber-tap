4ae5f8a167c78c331efbc05096a6f096d3480fe9
Calavoow <Calavoow@users.noreply.github.com>
2014-12-11 16:00:25 +0100

    Manual implementation of manchester coding
    Still needs testing

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,17 +1,67 @@
-#include <Manchester.h>
+const unsigned int rate = 300; // bit/s
+const unsigned int halfBit = 1/rate * 1000000; // delay in microseconds per half-bit, i.e. per transition.
+// The wait time in microseconds until to the middle of the next flank.
+// We wait until we are in the middle of a transmission flank.
+const unsigned int threeQuartBit = 1/rate * 1000000 * 1.5;
+
 // Photo sensor pin
-int photoSensor = A0;
+const int photoSensor = A0;
+const int threshold = 75;
 
 void setup() {
 	Serial.begin(9600);
 
-	// Use analog pin as digital
-	man.setupReceiveAnalog(photoSensor, MAN_300, 50);
 	// Wait for Serial to connect to so output.
 	while(!Serial);
 	Serial.println("begin");
 
-	man.beginReceive();
+}
+
+// Read the current sensor value
+bool readSensor() {
+	return analogRead(photoSensor) >= threshold;
+}
+
+// low-high is 1, high-low is 0.
+bool readTransition() {
+	bool curVal = readSensor();
+	bool nextVal = curVal;
+	while(nextVal == curVal) {
+		nextVal = readSensor();
+	}
+	return nextVal; //if true, it was low-high -> 1
+}
+
+byte readByte() {
+	byte readByte = 0x00;
+	for(int i=0; i < 8; i++){
+		bool val = readTransition();
+		// Writes lowest-significance first.
+		bitWrite(readByte, i, val);
+		delayMicroseconds(threeQuartBit); // Wait for half of the next bit transition.
+	}
+	return readByte;
+}
+
+const byte sync[] = {0xCA, 0xFE, 0xBA, 0xBE};
+// receive bytes number of bytes.
+void receive(int bytes, byte* values) {
+	// Sync must succeed first.
+	int syncCounter = 0;
+	while(syncCounter < sizeof(sync)) {
+		byte val = readByte();
+		if(val == sync[syncCounter]) {
+			syncCounter++;
+		} else {
+			// Reset counter, wrong byte received.
+			syncCounter = 0;
+		}
+	}
+
+	// When synced receive bytes.
+	for(int i=0; i < bytes; i++) {
+		values[i] = readByte();
+	}
 }
 
 // Buffer the received messages
@@ -23,18 +73,8 @@ int msg_counter = 0;
  * The main receiver program loop.
  **/
 void loop() {
-	// Receive data packets.
-	if(man.receiveComplete()) {
-		char m = man.getMessage();
-
-		password[msg_counter] = m;
-		msg_counter++;
-		// Print received messages
-		if(msg_counter > 24){
-		    Serial.println(password);
-		    msg_counter = 0;
-		}
-		// Get ready to receive next packet
-		man.beginReceive();
-	}
+	byte val[1];
+	receive(1, val);
+	char c = (char) val[0];
+	Serial.print(c);
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,25 +1,49 @@
-#include <Manchester.h>
+const unsigned int rate = 300; // bit/s
+const unsigned int halfBit = 1/rate * 1000000; // delay in microseconds per half-bit, i.e. per transition.
 // LED pin
-int led = 4;
+const int led = 4;
 
 void setup() {
 	Serial.begin(9600);
-  
-	man.setupTransmit(led, MAN_300);
 	// Wait for Serial to connect to output.
 	while(!Serial) ;
 	Serial.println("begin");
 }
 
-char password[26] = "correctHorseBatteryStaple";
-int counter = 0;
-void loop() {
-	// Transmit some data
-	Serial.println(password);
-  
-	man.transmit(password[counter]);
+void sendBit(bool bit) {
+	if(bit) { // 1 => low -> high
+		digitalWrite(led,  LOW);
+		delayMicroseconds(halfBit);
+		digitalWrite(led, HIGH);
+		delayMicroseconds(halfBit);
+	} else { // 0 => high -> low
+		digitalWrite(led,  HIGH);
+		delayMicroseconds(halfBit);
+		digitalWrite(led, LOW);
+		delayMicroseconds(halfBit);
+	}
+}
 
-	// Set next character
-	counter++;
-	if(counter >= strlen(password)) counter = 0;
+void sendByte(byte value) {
+	for(int i=0; i < 8; i++){
+		sendBit(bitRead(value, i));
+	}
+}
+
+const byte sync[] = {0xCA, 0xFE, 0xBA, 0xBE};
+void send(int bytes, byte* data) {
+	// First send the sync bits
+	for(int i = 0; i < sizeof(sync); i++) {
+		sendByte(sync[i]);
+	}
+
+	// Then send the data
+	for(int i = 0; i<bytes; i++){
+		sendByte(data[i]);
+	}
+}
+
+const char password[26] = "correctHorseBatteryStaple";
+void loop() {
+	send(sizeof(password), (byte*) password);
 }
